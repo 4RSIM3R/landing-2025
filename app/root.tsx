2fd83cb9514c06293a73aa956148faa9167ecf13
4RSIM3R <ilzammulkhaq85@gmail.com>
2025-05-29 12:37:07 +0700

    feat: footer dulu

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
@@ -18,7 +19,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap",
   },
 ];
 
@@ -32,7 +33,119 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <nav>Navbar Here</nav>
         {children}
+        <footer className="w-full bg-primary text-white">
+          <div className="grid grid-cols-12">
+            <div className="col-span-12 md:col-span-3 border border-white border-opacity-20 p-6 md:p-8">
+              <span className="text-base">Company</span>
+              <ul className="mt-2 space-y-0.5">
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Works
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-span-12 md:col-span-3 border border-white border-opacity-20 p-6 md:p-8">
+              <span className="text-base">Services</span>
+              <ul className="mt-2 space-y-0.5">
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Web Development
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Mobile Development
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    AI & Automation
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    IT Maintanance
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-span-12 md:col-span-3 border border-white border-opacity-20 p-6 md:p-8">
+              <span className="text-base">Resources</span>
+              <ul className="mt-2 space-y-0.5">
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Case Studies
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-span-12 md:col-span-3 border border-white border-opacity-20 p-6 md:p-8">
+              <span className="text-base">Follow Us</span>
+              <ul className="mt-2 space-y-0.5">
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Linkedin
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Instagram
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Facebook
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-lg hover:underline" to="/">
+                    Github
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-span-12 p-6 md:p-8">
+            <div className="flex flex-col md:flex-row items-center justify-center">
+              <p>
+                Copyright © Nexteam Teknologi Indonesia. All right reserved.
+              </p>
+            </div>
+          </div>
+        </footer>
         <ScrollRestoration />
         <Scripts />
       </body>
